dbce05031195a2a4ff18324a9316870b2b6e3c8d
Yesi Meza <yesi.meza10@gmail.com>
2021-02-16 23:49:45 -0700

    added test to create ,update,and delete

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -25,19 +25,71 @@ describe "Items API" do
       expect(item[:attributes][:unit_price]).to be_a(Numeric)
     end 
   end
-  # it "can get one merchant by its id" do 
-  #   id = create(:merchant).id 
+  it "can get one items by its id" do 
+    id = create(:item).id 
 
-  #   get "/api/v1/merchants/#{id}"
+    get "/api/v1/items/#{id}"
 
-  #   merchant = JSON.parse(response.body, symbolize_names: true)
+    item = JSON.parse(response.body, symbolize_names: true)
 
-  #   expect(response).to be_successful
+    expect(response).to be_successful
 
-  #   expect(merchant[:data][:attributes]).to have_key(:id)
-  #   expect(merchant[:data][:attributes][:id]).to eq(id)
+    expect(item[:data][:attributes]).to have_key(:id)
+    expect(item[:data][:attributes][:id]).to eq(id)
 
-  #   expect(merchant[:data][:attributes]).to have_key(:name)
-  #   expect(merchant[:data][:attributes][:name]).to be_a(String)
-  # end
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Numeric)
+  end
+  it "can create a new item" do 
+    merchant_id = create(:merchant).id 
+
+    item_params = ({
+                  name: 'ipad',
+                  description: 'is a mini computer', 
+                  unit_price: 150.00, 
+                  merchant_id: merchant_id
+                  })
+
+    headers = {"CONTENT_TYPE" => "application/json"}  
+      
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(merchant_id)
+  end
+  it "can update an existing item" do 
+    id = create(:item).id 
+    previous_name = Item.last.name
+    item_params = { name: "ipod nano" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+    
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("ipod nano")
+  end
+  it "can destory an item" do 
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    expect { delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end 

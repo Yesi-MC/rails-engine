8d9d608ec35fdd08da40ffdc6a866a71b4442e96
Yesi Meza <yesi.meza10@gmail.com>
2021-02-18 22:03:34 -0700

    added the find merchants and clean up page limit

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,24 @@
 class Api::V1::MerchantsController < ApplicationController
-
+  RESULTS_PER_PAGE = 20
+  
   def index
-    render json: MerchantSerializer.new(Merchant.all)
+    if params[:page]
+      page_num = params[:page].to_i - 1
+      merchants = Merchant.offset(page_num * RESULTS_PER_PAGE).limit(RESULTS_PER_PAGE)
+      render json: MerchantSerializer.new(merchants)
+    else 
+      merchants = Merchant.limit(RESULTS_PER_PAGE)
+      render json: MerchantSerializer.new(merchants)
+    end 
   end
 
   def show 
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def find
+    merchant = Merchant.where("lower(name) like ?", "%#{params[:name]}%".downcase).first
+    render json: MerchantSerializer.new(merchant)
+  end
 end
+

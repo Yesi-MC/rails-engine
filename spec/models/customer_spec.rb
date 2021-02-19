426572cf686c3d6318f361b279f924c2a714a172
Yesi Meza <yesi.meza10@gmail.com>
2021-02-18 22:04:25 -0700

    created model test validations/relations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,9 +5,4 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of :first_name}
     it { should validate_presence_of :last_name}
   end
-
-  # describe 'relationships' do
-  #   it {should have_many :transactions}
-  #   it {should have_many :invoice_items}
-  #   it {should have_many(:items).through(:invoice_items)}
 end

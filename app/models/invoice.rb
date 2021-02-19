677581e8ef341dd86756dd26e3b8866accda94e2
Yesi Meza <yesi.meza10@gmail.com>
2021-02-19 00:15:24 -0700

    added relationship

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,11 +1,12 @@
 class Invoice < ApplicationRecord
-  validates_presence_of :customer_id, :merchant_id 
+  validates_presence_of :customer_id, :merchant_id
 
   enum status: ['shipped', 'not shipped']
 
   belongs_to :customer
   belongs_to :merchant
 
+  has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
 end
